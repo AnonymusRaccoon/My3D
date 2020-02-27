a30d2034fdf7c334f2e353fb6b2f04416ade8ea9
AnonymusRaccoon <tr.tristan-roux@outlook.fr>
2020-02-27 17:27:11 +0100

    Adding good  texts for the PMs

--- src/main_menu.c
+++ src/main_menu.c
@@ -11,7 +11,8 @@ bool start_button(gc_engine *engine, int entity_id)
 	gc_scene *scene = scene_create(engine, "prefabs/game.gcprefab");
 	if (!scene) {
 		engine->should_close = true;
-		my_printf("The game scene couldn't be found.\n");
+		my_printf("The game scene couldn't be loaded.\n");
+		return (false);
 	}
 	engine->change_scene(engine, scene);
 	return (true);
