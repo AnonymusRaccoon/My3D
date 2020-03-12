b4d37eabf1a92cd5f3a817ed52f60d683856b65b
Clément Le Bihan <clement.le-bihan@epitech.eu>
2020-03-12 10:37:20 +0100

    adding texture loading from xml

--- include/map_interactions.h
+++ include/map_interactions.h
@@ -13,6 +13,15 @@
 #include "vertex_component.h"
 #include "keybindings.h"
 
+enum modes_on_tile {
+	VERTEX_0 = 2,
+	VERTEX_1 = 4,
+	VERTEX_2 = 8,
+	VERTEX_3 = 16,
+	ALL_VERTICES = 30,
+	INVERT_ADD_VALUE = 1
+};
+
 bool tile_interact(gc_engine *engine, gc_entity *entity, gc_vector2 pos, \
 enum gc_mousekeys key);
 
